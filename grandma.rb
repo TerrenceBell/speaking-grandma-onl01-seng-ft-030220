a7fe66b0b9a481b1b75cbdac87b88645d997278c
Terrence Bell <terrencekbell@gmail.com>
2020-03-02 23:37:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase
-    print 'HUH?! SPEAK UP, SONNY!'
+    return 'HUH?! SPEAK UP, SONNY!'
   elsif phrase == phrase.upcase
-  print 'NO, NOT SINCE 1938!'
+  return 'NO, NOT SINCE 1938!'
 else phrase == 'I LOVE YOU GRANDMA!'
-  print 'I LOVE YOU TOO PUMPKIN!'
+  return 'I LOVE YOU TOO PUMPKIN!'
 end
 end
